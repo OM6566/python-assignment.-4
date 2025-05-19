3c43ff1dc3136881930dbd9a71902751ec1cf103
OM6566 <ou.bhavsar2503@gmail.com>
2025-05-19 11:51:28 +0530

    Add files via upload

---
+++ task4.2.py
@@ -0,0 +1,17 @@
+user_input = input('Enter a text to write to the file: ')
+file = open('output.txt', 'w')
+file.write(user_input + '\n')
+file.close()
+print('Data successfully written to output.txt')
+
+add_input = input('Enter additional text to append: ')
+file = open('output.txt', 'a')
+file.write(add_input + '\n')
+file.close()
+print('Data successfully appended.')
+
+file = open('output.txt', 'r')
+content_file = file.read()
+file.close()
+print("\nFinal file content:")
+print(content_file)
